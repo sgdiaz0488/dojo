1b9c6745b06a4f72110127462d6d4d9a7928aed3
Sandra Gonzalez Diaz <sandry0488@hotmail.com>
2020-02-19 17:32:03 -0600

    .

--- backend-training/ruby-training/week_one/05/practice.rb
+++ backend-training/ruby-training/week_one/05/practice.rb
@@ -21,7 +21,7 @@ puts countries_content.count 'a'
 # Product
 # PaymentType
 #
-# The class names above are just simple examples, but you can use other ones
+#The class names above are just simple examples, but you can use other ones
 require 'csv'
 
 class Report
