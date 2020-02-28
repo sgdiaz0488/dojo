c2a85591d9a2e883df5d9a724e469ff2eab74389
Sandra Gonzalez Diaz <sandry0488@hotmail.com>
2020-02-28 10:55:01 -0600

    Solution for test 07

--- backend-training/ruby-training/week_one/test_exercises/07/lib/puppy.rb
+++ backend-training/ruby-training/week_one/test_exercises/07/lib/puppy.rb
@@ -1,26 +1,35 @@
+# frozen_string_literal: true
+
 class Puppy
   # Creates an instance variable "state" that you can set in your class.
   # "_reader" means other objects can read your state by calling "some_puppy.state".
-  attr_reader :state
+  attr_reader :state, :speak
 
   def initialize
     # Puppies are calm at first.
     @state = :calm
+    @speak = 'Bark!'
   end
 
   def pet
-    # Your code here
+    if @state == :calm
+      @state = :wagging
+    elsif @state == :wagging
+      @state = :excited
+    end
   end
 
   def rub_belly
-    # Your code here
+    @state = :calm
   end
 
   def spray
-    # Your code here
-  end
-
-  def speak
-    # Your code here
+    if @state == :calm
+      @state = :growling
+      @speak = 'Grrrr.'
+    elsif @state == :growling
+      @state = :angry
+      @speak = 'BARK BARK BARK!'
+    end
   end
 end
