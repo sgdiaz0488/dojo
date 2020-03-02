7cb7ffee3f868ba741aa3c3454ec064d913eee70
Sandra González Díaz <58184612+sgdiaz0488@users.noreply.github.com>
2020-03-01 22:43:05 -0600

    Solution for test 09 (#17)
    * Implementing change machine
    
    * Refactoring code

--- backend-training/ruby-training/week_one/test_exercises/09/lib/change_machine.rb
+++ backend-training/ruby-training/week_one/test_exercises/09/lib/change_machine.rb
@@ -1,19 +1,59 @@
+# frozen_string_literal: true
+
 # You can modify this class however you like to make it pass the test suite.
+
 class ChangeMachine
-  def initialize
-    # Your code here
-  end
+  attr_reader :quarter, :dime, :nickel, :penny
+
+  QUARTER = 25
+  DIME = 10
+  NICKEL = 5
+  PENNY = 1
 
   def cost(cents)
-    # Your code here
+    @cost = cents
   end
 
   def paid(cents)
-    # Your code here
+    @paid = cents
   end
 
   def dispense_change
-    # Your code here
+    amount = @paid - @cost
+    if @paid == @cost || amount > 199 || @paid < @cost
+      'non-dispensing change machine'
+    else
+      amount = amount > 100 ? amount - 100 : amount
+      change(amount)
+    end
+  end
+
+  def change(amount)
+    remaining_amount = amount
+
+    while remaining_amount != 0
+      coin = get_highest_coin(remaining_amount.ceil(3))
+
+      if coin == QUARTER
+        quarter
+      elsif coin == DIME
+        dime
+      elsif coin == NICKEL
+        nickel
+      elsif coin == PENNY
+        penny
+      end
+      remaining_amount -= coin
+    end
+  end
+
+  def get_highest_coin(amount)
+    coin = 0
+    [25, 10, 5, 1].each do |c|
+      coin = c
+      return coin if coin <= amount
+    end
+    coin
   end
 
   # You don't have to modify anything under this line
@@ -21,12 +61,15 @@ class ChangeMachine
   def quarter
     puts 'Dispensed quarter'
   end
+
   def dime
     puts 'Dispensed dime'
   end
+
   def nickel
     puts 'Dispensed nickel'
   end
+
   def penny
     puts 'Dispensed penny'
   end
