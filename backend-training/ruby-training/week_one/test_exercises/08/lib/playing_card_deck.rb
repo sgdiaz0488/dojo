b338574a00dcd4c87b456c8afdbfbc1c522e2b49
Sandra Gonzalez Diaz <sandry0488@hotmail.com>
2020-03-01 23:52:45 -0600

    Solution for test 08

--- backend-training/ruby-training/week_one/test_exercises/08/lib/playing_card_deck.rb
+++ backend-training/ruby-training/week_one/test_exercises/08/lib/playing_card_deck.rb
@@ -1,13 +1,43 @@
+# frozen_string_literal: true
+
 require_relative './deck'
 require_relative './playing_card'
 
 class PlayingCardDeck < Deck
+  SUITS = %w[clubs spades hearts diamonds].freeze
+  VALUES = %w[2 3 4 5 6 7 8 9 10 J Q K A].freeze
+
   def initialize
     super
-    # your code here
+    @cards = create_deck
+    shuffle
+  end
+
+  def create_deck
+    deck = []
+
+    SUITS.each do |suit|
+      VALUES.each do |value|
+        deck << PlayingCard.new(suit, value)
+      end
+    end
+
+    deck
   end
 
   def deal_poker_hands(count)
-    # your code here
+    poker_hands = []
+
+    count.times do
+      hand = []
+
+      5.times do
+        hand << deal
+      end
+
+      poker_hands << hand
+    end
+
+    poker_hands
   end
 end
